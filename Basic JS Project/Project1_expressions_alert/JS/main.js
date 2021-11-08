4ac345c561c6e36f61512fe82e840eeba2dcb4ae
techwolfd <93291605+techwolfd@users.noreply.github.com>
2021-11-08 12:08:06 -0800

    js basic

---
+++ Basic JS Project/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,28 @@
+
+//var a = "este es un valor string wolf";
+//window.alert(a);
+//alert("Hello, \"ejemplo de comillas\"Wolf!");
+
+//var b = "concatenacion"+" para mostrar" +" una concatenacion con +";
+//document.write(b);
+
+//var c = "concatenacion", d =" para mostrar", e =" una concatenacion con +";
+//document.write(e);
+
+var azul = "wolf";
+var azul = azul.fontcolor("green");
+document.write(azul);
+
+var yellow = "wolf";
+var yellow = yellow.fontcolor("yellow");
+document.write(yellow);
+
+
+document.wri
+
+function My_First_Function(){
+    var str = "texto en verde";
+    var resultado = str.fontcolor("green");
+    document.getElementById("Green_Text").innerHTML=resultado;
+    
+}
