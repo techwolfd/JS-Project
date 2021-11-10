fe447115b6083fabb734dcbd7556e246ab19270a
techwolfd <93291605+techwolfd@users.noreply.github.com>
2021-11-10 10:46:00 -0800

    js8

--- Basic JS Project/Project8_string_methods/JS/main8.js
+++ Basic JS Project/Project8_string_methods/JS/main8.js
@@ -1,16 +1,31 @@
 
  function fullsentence(){
-  var parte1 = "tengo ";
-  var parte2 = "echo en ";
-  var parte3 = "dentro un completo ";
-  var parte4 = "sentencia ";
-  var whole = parte1.concat(parte2,parte3,parte4);
+  var parte1 = "tengo ";//string1
+  var parte2 = "echo en ";//string2
+  var parte3 = "este ejemplo una ";//string3
+  var parte4 = "concatenacion ";//string4
+  var whole = parte1.concat(parte2,parte3,parte4);//concatenacion
   document.getElementById("concatenar").innerHTML=whole;
 
  }
 
  function slicemetodo(){
-     var sentencia = "todo al trabajo y no jugar haciendo a johnny un buen chico";
-     var seccion = sentencia.slice(27,33);
+     var sentencia = "todo al trabajo y no jugar haciendo a johnny un buen chico";//string
+     var seccion = sentencia.slice(27,33);//rango de posiciones
      document.getElementById("slice").innerHTML=seccion;
  }
+
+ function mfupper() {
+    let text = document.getElementById("upper").innerHTML;
+    document.getElementById("upper").innerHTML =    text.toUpperCase();//funcion para uppercase
+  }
+
+  function metstr(){
+      var x = 182;
+      document.getElementById("nstr").innerHTML=x.toString();//funcion tostring
+  }
+
+  function metpresicion(){
+      var x= 12345.67891123;
+      document.getElementById("presicion").innerHTML=x.toPrecision(9);//funcion presicion a un numero
+  }
