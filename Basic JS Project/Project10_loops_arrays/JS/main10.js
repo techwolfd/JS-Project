e69a45e46719276f57d008d9b8d497ba45e025c7
techwolfd <93291605+techwolfd@users.noreply.github.com>
2021-11-11 10:52:26 -0800

    10

--- Basic JS Project/Project10_loops_arrays/JS/main10.js
+++ Basic JS Project/Project10_loops_arrays/JS/main10.js
@@ -1,7 +1,7 @@
 alert(document.getElementById("p1").innerHTML);
 
 
-a="siempre ";
+a="siempcc----cre ";
 b="recordar ";
 document.write(a+b);
 
@@ -9,10 +9,26 @@ x="tu ";
 y="eres ";
 document.write(x+y);
 
+
+
+
+
+var d=82;
+document.write("<br>"+d +"here");
+{
+    let d =33 ;
+    document.write("<br>"+d);
+}
+document.write("<br>"+d);
+{
+    var d =33 ;
+    document.write("<br>"+d);
+}
+document.write("<br>"+d);
+
 m="unico ";
 n="como ninguno ";
 document.write(m+n);
-
 function contara10(){
     var digito ="";
     var x = 1;
@@ -34,11 +50,26 @@ function listnumber(){
     document.getElementById("listnumber").innerHTML=content;
 }
 
-function imgcats(){
-    var cat_pic=[];
-    cat_pic=[0] = "durmiendo";
-    cat_pic=[1] = "jugar";
-    cat_pic=[2] = "comer";
-    document.getElementById("gato").innerHTML=cat_pic[1] ;
 
+function imgcats() {
+    var cat_pic= [];
+    cat_pic[0] = "durmiendo";
+    cat_pic[1] = "jugando";
+    cat_pic[2] = "comiendo";
+    document.getElementById("gato").innerHTML="el gato esta  " + cat_pic[1] + "." ;
+
+}//
+
+
+
+
+function constantef(){
+    const instmusical ={type:"frand",brand:"fender",color:"black"};
+    instmusical.color="blue";
+    instmusical.price="$900";
+    document.getElementById("constante").innerHTML="el costo de "+instmusical.type+" fue "+instmusical.price;
 }
+
+
+
+
